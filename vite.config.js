6166398c91dfe12abf849ac11b4804f3bfc86276
Rohan Gope <rg2512@it.jgec.ac.in>
2024-07-25 14:40:48 +0530

    Update api end point

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	server: {
 		proxy: {
-			'/api': 'https://mess-manager-backend.onrender.com',
+			'/api': 'https://mess-manager-backend-1.onrender.com/',
 		},
 	},
 	plugins: [react()],
